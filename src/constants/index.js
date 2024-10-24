d4d98b083ed93adbbce20bc09f5d0c0d798b5f1b
YADAV Vishwaraj <VYV2@dsone.3ds.com>
2024-10-24 16:16:43 +0530

    feat: updated constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,23 +2,23 @@ import project1 from "../assets/projects/project-1.webp";
 import project2 from "../assets/projects/project-2.webp";
 import project3 from "../assets/projects/project-3.webp";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate front end developer with a knack for crafting robust and scalable web applications. With 5+ years of hands-on experience, I have honed my skills in front-end technologies like React, Redux, Javascript, HTML, CSS and CSS Frameworks like Bootstrap and Tailwind. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for creating efficient and user-friendly web applications. With 5+ years of professional experience, I have worked with a variety of technologies, including React, Redux, Javascript, HTML, CSS and CSS Frameworks like Bootstrap and Tailwind. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and solving coding questions.`;
 
 export const EXPERIENCES = [{
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2021 - Present",
+    role: "Research and Development Specialist",
+    company: "Dassault Systemes",
+    description: `Led a team in developing and maintaining an application within the 3DS cloud infrastructure for managing real -world locations by creating their virtual counterparts. Enabled the creation of virtual twins of physical locations, facilitating operations in various 3D tools for area measurements, object placement simulations, and other 3D functionalities. `,
+    technologies: ["React", "Javascript", "HTML", "CSS", "REST APIs", "NodeJS", "MongoDB"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "2019 - 2021",
+    role: "System Engineer",
+    company: "Tata Consultancy Services",
+    description: `Played a key role in the transformation of the admin website post-merger of BB&T and SunTrust to form Truist, using React to modernize and enhance functionality. Worked closely with backend developers to integrate frontend components with REST APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["React", "Javascript", "HTML", "CSS", "Tailwind CSS", "REST APIs"],
   },
 ];
 
@@ -29,7 +29,7 @@ export const PROJECTS = [{
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "Tournament Management App",
     image: project2,
     description: "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
@@ -38,7 +38,7 @@ export const PROJECTS = [{
     title: "Portfolio Website",
     image: project3,
     description: "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "Javascript", "React", "Tailwind CSS", "Framer Motion"],
   },
 ];
 
